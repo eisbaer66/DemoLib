6c23616dca784c25f8821169dd2489abbd04329e
Jan <janweidner87@gmail.com>
2017-03-23 03:02:46 +0100

    PushTo-GitHub generates release text from exe;

--- util/PushTo-GitHub.ps1
+++ util/PushTo-GitHub.ps1
@@ -1,41 +1,35 @@
 ﻿param(
 [string]$GitHubToken,
 [string]$ReleaseName,
-[string]$ZipLocation)
+[string]$TagName,
+[string]$Version,
+[string]$ZipLocation,
+[string]$ExeLocation)
 
 $ErrorActionPreference = "Stop"
 
 $authheader = "Basic " + ([Convert]::ToBase64String([System.Text.encoding]::ASCII.GetBytes($GitHubToken)))
 $header = @{Authorization=$authheader}
 
+$stdErrLog = "stderr.log"
+$stdOutLog = "stdout.log"
+Start-Process -File $ExeLocation  -ArgumentList "-h" -RedirectStandardOutput $stdOutLog -RedirectStandardError $stdErrLog -wait
+$output = (Get-Content $stdErrLog, $stdOutLog | out-string)
+$output
+
 $text = @"
-``` 
-PurgeDemoCommands $ReleaseName
-
-Usage: PurgeDemoCommands.exe awesome.dem other.dem
-       PurgeDemoCommands.exe "C:\path\to\demos\awesome.dem"
-       PurgeDemoCommands.exe "C:\path\to\demos"
-       PurgeDemoCommands.exe awesome.dem -s _clean
-       PurgeDemoCommands.exe awesome.dem -o
-
-Options:
-
-  -s, --suffix               (Default: _purged) suffix of generated file
-
-  -t, --skipTest             (Default: False) skips test if purged file can be parsed again
-
-  -o, --overwrite            (Default: False) overwrites existing (purged) files
-
-  -p, --successfullPurges    (Default: False) shows a summary after purgeing
-
-  --help                     Display this help screen.
-``` 
+``````
+$output
+``````
 "@
-$body = "{""tag_name"": ""$ReleaseName"",""target_commitish"": ""eisbaer"",""name"": ""$ReleaseName"",""body"": ""$text"",""draft"": true,""prerelease"": true}"
+$text= ConvertTo-Json $text
+$body = "{""tag_name"": ""$TagName"",""target_commitish"": ""purge"",""name"": ""$ReleaseName"",""body"": $text,""draft"": true,""prerelease"": true}"
+
+write-output $body
 
 $response = Invoke-RestMethod -Method Post -Uri https://api.github.com/repos/eisbaer66/PurgeDemoCommands/releases -Body $body -Headers $header;
 $uploadUrl = $response.upload_url
 $indexEndOfUploadUrl = $uploadUrl.IndexOf("{")
 $uploadUrl = $uploadUrl.Substring(0, $indexEndOfUploadUrl)
-$uploadUrl = $uploadUrl + "?name=TF2ItemsEditor.zip"
+$uploadUrl = $uploadUrl + "?name=PurgeDemoCommands_$ReleaseName.zip"
 Invoke-RestMethod -Method Post -Uri $uploadUrl -InFile $zipLocation -Headers $header -ContentType "application/zip";
